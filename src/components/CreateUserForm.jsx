d6f822b6a0aff83d24bba5b4b1a2faf03e61e309
ade-t <ad_elyn@hotmail.com>
2023-04-18 14:11:37 +0800

    add password field

--- src/components/CreateUserForm.jsx
+++ src/components/CreateUserForm.jsx
@@ -1,20 +1,22 @@
 import { useState } from 'react';
 
-function CreateUserForm() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-
-
+export default function CreateUserForm() {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      const response = await fetch(`${process.env.SERVER}/api/users`, {
-        method: 'POST',
+      const response = await fetch("/api/users", {
+        method: "POST",
         headers: { 
-          'Content-Type': 'application/json' 
+          "Content-Type": "application/json"
         },
-        body: JSON.stringify({ name, email }),
+        body: JSON.stringify(user),
       });
       const data = await response.json();
       console.log(data);
@@ -23,27 +25,38 @@ function CreateUserForm() {
     }
   };
 
+  const handleChange = (event) => {
+    setUser({ ...user, [event.target.name]: event.target.value});
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <label>
         Name:
         <input
-          type="text"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
+          name="name"
+          value={user.name} required
+          onChange={handleChange}
         />
       </label>
       <label>
         Email:
         <input
-          type="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
+          name="email"
+          value={user.email} required
+          onChange={handleChange}
         />
       </label>
+      <label>
+      Password:
+      <input
+        name="password"
+        value={user.password} required
+        onChange={handleChange}
+      />
+    </label>
       <button type="submit">Create User</button>
     </form>
   );
 }
 
-export default CreateUserForm
