7ee646a3599d9913807cd265d64523b8b3ef1ae7
ade-t <ad_elyn@hotmail.com>
2023-04-22 15:44:31 +0800

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./index.html", "./src/**/*.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
