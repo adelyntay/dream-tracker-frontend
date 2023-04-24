b059348bf07fdb1b948635bba2fdd03f88947082
ade-t <ad_elyn@hotmail.com>
2023-04-24 14:51:35 +0800

    add tailwind color preset

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./index.html", "./src/**/*.jsx"],
   theme: {
-    extend: {},
+    extend: {
+  },
+  colors: {
+      orange: "#ff8906",
+      red: "#f25f4c",
+      pink: "#e53170",
+      black: "#0f0e17",
+      white: "#fffffe"
+    },
   },
   plugins: [],
 };
