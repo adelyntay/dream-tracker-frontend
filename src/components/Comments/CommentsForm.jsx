2bff1e213baf18ae9d2be205d379eeed4fb4b950
ade-t <ad_elyn@hotmail.com>
2023-04-25 15:41:51 +0800

    show comments below post

--- src/components/Comments/CommentsForm.jsx
+++ src/components/Comments/CommentsForm.jsx
@@ -1,5 +1,6 @@
 import { useForm } from "react-hook-form"
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from "react";
 
 export default function CommentsForm () {
     const { id } = useParams();
@@ -9,13 +10,17 @@ export default function CommentsForm () {
         handleSubmit,
         formState: {errors},
     } = useForm();
+
+    const [comments, setComments] = useState([]);
     
     const onSubmit = async (data) => {
         try {
-            const response = await fetch(`/api/posts/${id}`, {
+            const token = localStorage.getItem("token")
+            const response = await fetch(`/api/posts/${id}/comments`, {
                 method: "POST",
                 headers: {
                   "Content-Type": "application/json",
+                  "Authorization": `Bearer ${token}`,
                 },
                 body: JSON.stringify(data),
             });
@@ -28,16 +33,47 @@ export default function CommentsForm () {
         }
     };
 
+    const getComments = async () => {
+        try {
+          const response = await fetch(`/api/posts/${id}/comments`);
+          if (response.ok) {
+            const responseData = await response.json();
+            setComments(responseData);
+          }
+        } catch (error) {
+          console.error(error);
+        }
+
+      setTimeout(getComments, 5000)
+    };
+
+    useEffect(() => {
+        getComments();
+    },[]);
+
     return (
+        <div>
         <form onSubmit={handleSubmit(onSubmit)}>
         <textarea
             {...register("comment", {
                 required: true})}
                 placeholder="write your comment"
+                className="text-black"
             />
             {errors.comment && <p>empty post</p>}
             <button type="submit">Submit</button>
         </form>
+        <ul>
+        {comments.map((comment) => (
+            <div>
+            <li key={comment._id}>{comment.comment}</li>
+            <li key={comment._id}>{comment.user.slice(0, 2) + 
+                '*'.repeat(comment.user.length - 4) + 
+                comment.user.slice(-2)}</li>
+            </div>
+        ))}
+        </ul>
+        </div>
     )
 }
 
