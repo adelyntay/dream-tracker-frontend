78fdca74929ba1ab72fe2420d6a1cbcf69e77c95
ade-t <ad_elyn@hotmail.com>
2023-04-26 22:21:05 +0800

    update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
 import { Routes, Route } from "react-router-dom";
-import CreateUserForm from "./components/Auth/CreateUserForm";
-import LoginPage from "./components/Auth/LoginForm";
+import LoginPage from "./pages/LoginPage";
 import NewDreamPage from "./pages/NewDreamPage";
 import HomePage from "./pages/HomePage";
 import DreamPage from "./pages/DreamPage";
 import WallPage from "./pages/WallPage";
 import CommentsPage from "./pages/CommentsPage";
 import InfoPage from "./pages/InfoPage";
-import EditPage from "./pages/EditPage"
-
+import EditPage from "./pages/EditPage";
+import SignUpPage from "./pages/SignUpPage"
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/wall" element={<WallPage />} />
-      <Route path="/create" element={<CreateUserForm />} />
+      <Route path="/create" element={<SignUpPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/newpost" element={<NewDreamPage />} />
       <Route path="/posts" element={<DreamPage />} />
       <Route path="/posts/:id/comment" element={<CommentsPage/>} />
       <Route path="/posts/:id" element={<InfoPage/>} />
       <Route path="/posts/:id/edit" element={<EditPage />} />
-      </Routes>
+    </Routes>
   )
 }
 
