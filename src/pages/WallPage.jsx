2c6297429de24d2d577a1dc34b800f099762c4cb
ade-t <ad_elyn@hotmail.com>
2023-04-27 21:53:40 +0800

    CSS

--- src/pages/WallPage.jsx
+++ src/pages/WallPage.jsx
@@ -5,6 +5,7 @@ export default function WallPage() {
     return(
         <>
         <NavBar />
+        <p className="font-bold text-center text-2xl text-pink">Morpheus Wall</p>
         <div className="grid grid-cols-2">
         <img src="https://media.giphy.com/media/0zOHG2RVocxnAj2SDz/giphy.gif" width="350" height="260" className="mt-10 gird col-start-1 pl-10 ml-10"/>
         <div className="mt-10">
