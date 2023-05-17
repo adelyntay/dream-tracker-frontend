679275aae11cd6fc18bd5f1684856c0f5b2fb200
ade-t <ad_elyn@hotmail.com>
2023-05-18 01:38:29 +0800

    update config

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     proxy: {
       // "/api": process.env.SERVER,
       // "/api": "http://127.0.0.1:5000",
-      "/api": {
+      "/": {
         target: process.env.REACT_APP_BACKEND_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
